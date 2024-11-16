abe17a8bebf5c4ad13442d42baffa8c72026aff7
blkkatana <coder.joey.256@gmail.com>
2024-11-16 13:07:42 -0500

    Modified install.py.

--- install.py
+++ install.py
@@ -7,6 +7,17 @@ KERNEL_TO_INSTALL = 'linux'
 subprocess.run(['clear'])
 print('Arch Install Script')
 
-print('Setting up Wi-Fi...')
-subprocess.run(['ip', 'link'])
-subprocess.run(['iwctl'])
+setupWifi = input('Are you using Wi-Fi? (y/n) ').strip()
+if setupWifi == 'y':
+    print('Setting up Wi-Fi...')
+    subprocess.run(['ip', 'link'])
+    subprocess.run(['iwctl'])
+else:
+    print('Setting up Ethernet...')
+    subprocess.run(['ip', 'link'])
+
+print('Testing connection...')
+print('Use Ctrl-C to exit.')
+subprocess.run(['ping', 'archlinux.org'])
+
+print('Install complete. Enjoy!')
